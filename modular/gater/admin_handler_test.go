2410931bb80461025334f4060b5deda1b0feef9d
phenix3443 <phenix3443@gmail.com>
2026-08-31 16:48:58 +0800

    Merge pull request #151 from mocachain/fix/moca-865-replicate-limit
    fix(gater): limit replicate request bodies
    
    (cherry picked from commit c9d900af80de51ed510f90469ce503dda0030dc4)

--- modular/gater/admin_handler_test.go
+++ modular/gater/admin_handler_test.go
@@ -1,6 +1,7 @@
 package gater
 
 import (
+	"bytes"
 	"context"
 	"errors"
 	"fmt"
@@ -25,6 +26,16 @@ import (
 	"github.com/mocachain/moca-storage-provider/core/consensus"
 	"github.com/mocachain/moca-storage-provider/core/piecestore"
 )
+
+func TestReadReplicateBodyLimit(t *testing.T) {
+	data, err := readReplicateBody(bytes.NewReader([]byte("12345")), 4)
+	assert.ErrorIs(t, err, ErrInvalidPayloadSize)
+	assert.Nil(t, data)
+
+	data, err = readReplicateBody(bytes.NewReader([]byte("1234")), 4)
+	assert.NoError(t, err)
+	assert.Equal(t, []byte("1234"), data)
+}
 
 func mockGetApprovalRoute(t *testing.T, g *GateModular) *mux.Router {
 	t.Helper()
