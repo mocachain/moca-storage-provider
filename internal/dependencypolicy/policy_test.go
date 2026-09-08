68c849a86bc8235551350fcf3697f6b039fd6a10
phenix3443 <phenix3443@gmail.com>
2026-09-08 16:37:24 +0800

    fix(deps): enable mocachain checksum verification (MOCA-920)

--- internal/dependencypolicy/policy_test.go
+++ internal/dependencypolicy/policy_test.go
@@ -2,23 +2,15 @@ package dependencypolicy
 
 import (
 	"os"
-	"regexp"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
 
-func TestPrivateModulesKeepChecksumVerificationEnabled(t *testing.T) {
+func TestMocachainModulesKeepChecksumVerificationEnabled(t *testing.T) {
 	dockerfile, err := os.ReadFile("../../Dockerfile")
 	require.NoError(t, err)
+	require.NotContains(t, string(dockerfile), "GOPRIVATE=github.com/mocachain")
 	require.NotContains(t, string(dockerfile), "GONOSUMDB=github.com/mocachain/*")
 	require.NotContains(t, string(dockerfile), "GONOSUMCHECK=github.com/mocachain/*")
-}
-
-func TestPrivateModuleReplacementsAreNotPinnedToPrereleaseTags(t *testing.T) {
-	goMod, err := os.ReadFile("../../go.mod")
-	require.NoError(t, err)
-
-	floatingPrerelease := regexp.MustCompile(`github\.com/mocachain/[^\s]+\s+v[^\s]+-rc\d+`)
-	require.Empty(t, floatingPrerelease.FindAllString(string(goMod), -1))
 }
